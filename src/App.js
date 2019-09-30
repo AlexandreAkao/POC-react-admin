fa372eb291579945ce07f582ae7b66d096dc5920
Alexandre Akira <alexandreakiraakao@gmail.com>
2019-09-30 14:53:59 -0300

    conexao com o Db no git

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+class App extends Component{
 
-const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+    constructor(props) {
+        super(props)
 
-const App = () => (
-    <Admin dataProvider={dataProvider}>
-        <Resource name="users" list={ListGuesser} />
-    </Admin>
-)
+        this.state = {
+            //dataProvider: jsonServerProvider('http://jsonplaceholder.typicode.com')
+            dataProvider: jsonServerProvider('http://my-json-server.typicode.com/AlexandreAkao/demo')
+
+        }
+    }
+
+    render() {
+        console.log(this.state.dataProvider)
+
+        return (
+            <Admin dataProvider={this.state.dataProvider}>
+                <Resource name="users" list={ListGuesser} />
+            </Admin>
+        )
+    }
+}
 
 export default App;
